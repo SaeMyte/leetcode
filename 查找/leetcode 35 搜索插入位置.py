ff7027fbcc222a8a62ac5d52d6bf4343449b6f0b
tonia <118867561+SaeMyte@users.noreply.github.com>
2022-12-01 23:00:57 +0800

    Create leetcode 35 搜索插入位置.py

---
+++ 查找/leetcode 35 搜索插入位置.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            mid = int((i+j)/2)
+            if nums[mid] >= target:
+                j = mid - 1
+            else:
+                i = mid + 1
+        return i
