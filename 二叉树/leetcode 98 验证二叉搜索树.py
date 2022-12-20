cb94dd21a2c15712844c7194385b35d00454930d
tonia <118867561+SaeMyte@users.noreply.github.com>
2022-12-20 11:34:23 +0800

    Update leetcode 98 验证二叉搜索树.py

--- 二叉树/leetcode 98 验证二叉搜索树.py
+++ 二叉树/leetcode 98 验证二叉搜索树.py
@@ -23,3 +23,27 @@ class Solution(object):
         if self.flag == 1:
             return True
         return False
+    
+    
+    
+import sys
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        self.preValue = -sys.maxint - 1  #记录前面的值，初始化为最小值
+        # 二叉搜索树中序一定是升序
+        def inorderTrasval(root):
+            if root == None:
+              return True  #空树也是二叉搜索树
+            isLeftBst = inorderTrasval(root.left)  # 左树是否是搜索二叉树
+            if not isLeftBst:  # 左树不是搜索二叉树，返回false
+                return False
+            if root.val > self.preValue: # 当前值比以前的值大，更新preValue
+                self.preValue = root.val
+            else:  # 否则，为false
+                return False
+            return inorderTrasval(root.right)
+        return inorderTrasval(root)
